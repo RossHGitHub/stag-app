a75106bfa7a0117482a03e9dd3a1404e8f657e25
Ross Henderson <rossliamhenderson@gmail.com>
2026-03-23 22:27:38 +0000

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/stag-app/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true
+  },
   server: {
     host: "127.0.0.1",
     port: 5173
